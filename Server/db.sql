f6d35b9a4355eda191f2a264e6ced6f3853eca1f
oledybedokken <69799492+oledybedokken@users.noreply.github.com>
2022-01-28 15:45:40 +0100

    up

---
+++ Server/db.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE bachelor;
+
+CREATE EXTENSION postgis;
+
+CREATE TABLE sources(
+    id VARCHAR(10) PRIMARY KEY UNIQUE NOT NULL,
+    type VARCHAR(50),
+    name VARCHAR(50) NOT NULL,
+    shortName VARCHAR(50),
+    country VARCHAR(50) NOT NULL,
+    countryCode VARCHAR(50),
+    long VARCHAR(50) NOT NULL,
+    lat VARCHAR(50) NOT NULL,
+    geog geography(point) NOT NULL,
+    valid_from TIMESTAMP
+);
+
