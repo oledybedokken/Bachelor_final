f0407a113f9e11db4a83ae2e2a558ee591762cea
oledybedokken <69799492+oledybedokken@users.noreply.github.com>
2022-01-28 16:47:14 +0100

    push

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ const db = require("./db");
 const fetch = require('node-fetch');
 
 const port = process.env.PORT || 3001;
-fetch('https://frost.met.no/sources/v0.jsonld?types=SensorSystem',{
+fetch('https://frost.met.no/sources/v0.jsonld?types=SensorSystem&country=Norge',{
     method:"get",
     body: JSON.stringify(),
     headers:{ Authorization: 'Basic YjVlNmEzODEtZmFjNS00ZDA4LWIwNjktODcwMzBlY2JkNTFjOg==' }
@@ -17,7 +17,7 @@ fetch('https://frost.met.no/sources/v0.jsonld?types=SensorSystem',{
         try{
         await db.query("DROP TABLE IF EXISTS sources;")
         await db.query("CREATE TABLE sources(id VARCHAR(10) PRIMARY KEY UNIQUE NOT NULL,type VARCHAR(50),name VARCHAR(60) NOT NULL,shortName VARCHAR(50),country VARCHAR(70) NOT NULL,countryCode VARCHAR(80),long VARCHAR(90) NOT NULL,lat VARCHAR(100) NOT NULL,geog geography(point) NOT NULL,valid_from TIMESTAMP);")
-        data.data.map(async (source)=>{
+        data.data.map(async(source)=>{
             if(source.geometry && source.geometry){
             let Point = `POINT(${source.geometry.coordinates[0]} ${source.geometry.coordinates[1]})`
             const results =await db.query("INSERT INTO sources(id,type,name,shortName,country,countryCode,long,lat,geog,valid_from) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) returning name;",[source.id,source.type,source.name,source.shortName,source.country,source.countryCode,source.geometry.coordinates[0],source.geometry.coordinates[1],Point,source.validFrom])
