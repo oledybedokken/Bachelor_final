8b5999edb971a19ff91ecb7b5e3fe829528c0f26
Mo <mussa80banjai@users.noreply.github.com>
2022-01-31 00:10:49 +0100

    Om filen burde være i .jsx? eller spiller ingen rolle

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,35 @@
 import './App.css';
 import routes from './routes';
 import {Routes, Route} from 'react-router-dom';
+import { SourceContextProvider } from './context/SourceContext';
 import { Suspense } from 'react';
+import Home from './Page/Home';
+import MapSide from './Page/MapSide';
 function App() {
-  return (
+/*   return (
     <div className="App">
       <Suspense fallback={<p>Loading...</p>}>
 
       <Routes>
         {routes.map((info,index)=>(
           <Route path={info.path} element={<info.comp/>} key={index}></Route>
+          <Route exact path ="/MapSide" element={<MapSide/>}></Route>
         ))}
       </Routes>
       </Suspense>
     </div>
+  ); */
+
+  return(
+    <SourceContextProvider>
+      <div className='container'>
+        <Routes>
+          <Route exact path ="/" element={<Home/>}/>
+          <Route exact path="/MapSide" element={<MapSide/>}/>
+        </Routes>
+
+      </div>
+    </SourceContextProvider>
   );
 }
 
