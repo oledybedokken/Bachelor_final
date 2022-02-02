9e84ab831e0dd5cf70583ff6ac379bd5e40f1edd
oledybedokken <69799492+oledybedokken@users.noreply.github.com>
2022-02-02 16:26:55 +0100

    Fix on hover

--- client/src/Page/Map/MapTest.jsx
+++ client/src/Page/Map/MapTest.jsx
@@ -1,1 +1,16 @@
 
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoib2xlZHliZWRva2tlbiIsImEiOiJja3ljb3ZvMnYwcmdrMm5vZHZtZHpqcWNvIn0.9-uQhH-WQmh-IwrA6gNtUg';
+
+const MapTest = () => {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+  return <div></div>;
+};
+
+export default MapTest;
--- client/src/Page/Map/MapView.jsx
+++ client/src/Page/Map/MapView.jsx
@@ -1,89 +1,133 @@
-import React, { useContext, useEffect, useRef,useState,useCallback } from 'react';
-import './MapViews.css';
-import { useNavigate } from 'react-router-dom';
-import ReactMapGL, { Marker, Layer, Source } from 'react-map-gl';
-import SourceFinder from '../../Apis/SourceFinder';
-import { SourceContext } from '../../context/SourceContext';
-import { clusterLayer, clusterCountLayer, unclusteredPointLayer } from './Layers';
+import React, {
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from "react";
+import "./MapViews.css";
+import { useNavigate } from "react-router-dom";
+import ReactMapGL, { Marker, Layer, Source, Popup } from "react-map-gl";
+import SourceFinder from "../../Apis/SourceFinder";
+import { SourceContext } from "../../context/SourceContext";
+import {
+  clusterLayer,
+  clusterCountLayer,
+  unclusteredPointLayer,
+} from "./Layers";
 const MapView = () => {
-  const navigate = useNavigate()
-  const [color,setColor]=useState("#11b4da") //Dette er bare midlertidig tror jeg
-  const { sources, setSources } = useContext(SourceContext)
-  const [selectedSource,SetSelectedSource] = useState(null)
+  const navigate = useNavigate();
+  const [color, setColor] = useState("#11b4da"); //Dette er bare midlertidig tror jeg
+  const { sources, setSources } = useContext(SourceContext);
+  const [hoverInfo, setHoverInfo] = useState(null);
   const [viewport, setViewport] = React.useState({
     longitude: 10.757933,
     latitude: 59.91149,
     zoom: 12,
     bearing: 0,
-    pitch: 0
+    pitch: 0,
   });
   const mapRef = useRef(null);
-  
-  function getCursor({isHovering, isDragging}) {
-    return isDragging ? 'grabbing' : isHovering ? 'pointer' : 'default';
+  function getCursor({ isHovering, isDragging }) {
+    return isDragging ? "grabbing" : isHovering ? "pointer" : "default";
   }
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await SourceFinder.get("/sources");
         setSources(response.data.data.plass);
-      } catch (error) { }
+      } catch (error) {}
     };
     fetchData();
-  }, [])
-  
+  }, []);
   const onMouseEnter = useCallback(event =>{
-    const source = event.features && event.features[0]
-    if (event.features[0].layer.id==="unclustered-point"){
-        setColor("#FFFF00")
+    if (event.features[0].layer.id === "unclustered-point"){
+      const hoverInfoNu = event.features[0].id //Må ha denne for å sørge for å oppdatere
+      setHoverInfo(event.features[0].id);
+      const map = mapRef.current.getMap();
+      map.setFeatureState({
+        source:"stasjoner",
+        id:hoverInfoNu
+      },
+      {hover:true})
     }
   })
-
-  const onMouseLeave=useCallback(event =>{
-      setColor("#11b4da")
-  })
-  const ShowMore = event => {
-    if(event.features[0].layer.id==="unclustered-point"){
+  const onMouseLeave = useCallback(event =>{
+    if(hoverInfo){
+      const map = mapRef.current.getMap();
+      map.setFeatureState({
+        source:"stasjoner",
+        id:hoverInfo
+      },
+      {hover:false})
+    }
+    }
+  );
+  const ShowMore = (event) => {
+    if (event.features[0].layer.id === "unclustered-point") {
       const feature = event.features[0];
-      console.log(feature)
-      mapRef.current.getMap().getCanvas().style.cursor="pointer"
-    }else{
-    const feature = event.features[0];
-    const clusterId = feature.properties.cluster_id;
-    const mapboxSource = mapRef.current.getMap().getSource('stasjoner');
-    mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
-      if (err) {
-        return;
-      }
-      setViewport({
-        ...viewport,
-        longitude: feature.geometry.coordinates[0],
-        latitude: feature.geometry.coordinates[1],
-        zoom,
-        transitionDuration: 500
+      mapRef.current.getMap().getCanvas().style.cursor = "pointer";
+    } else {
+      const feature = event.features[0];
+      const clusterId = feature.properties.cluster_id;
+      const mapboxSource = mapRef.current.getMap().getSource("stasjoner");
+      mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
+        if (err) {
+          return;
+        }
+        setViewport({
+          ...viewport,
+          longitude: feature.geometry.coordinates[0],
+          latitude: feature.geometry.coordinates[1],
+          zoom,
+          transitionDuration: 500,
+        });
       });
-    });
-  }
+    }
   };
+  const selectedSource = (hoverInfo && hoverInfo.name) || "";
   return (
-    <ReactMapGL  {...viewport} width="100%" height="100%" onMouseLeave={onMouseLeave} getCursor={getCursor} onMouseEnter={onMouseEnter} interactiveLayerIds={[clusterLayer.id,unclusteredPointLayer.id]} mapboxApiAccessToken={"pk.eyJ1Ijoib2xlZHliZWRva2tlbiIsImEiOiJja3ljb3ZvMnYwcmdrMm5vZHZtZHpqcWNvIn0.9-uQhH-WQmh-IwrA6gNtUg"} clickRadius={2} onViewportChange={setViewport} mapStyle="mapbox://styles/mapbox/streets-v11" onClick={ShowMore} ref={mapRef}>
-      <Source id="stasjoner" type="geojson" data={sources} cluster={true} clusterMaxZoom={14} clusterRadius={50}  >
+    <ReactMapGL
+      {...viewport}
+      width="100%"
+      height="100%"
+      onMouseLeave={onMouseLeave}
+      getCursor={getCursor}
+      interactiveLayerIds={["unclustered-point", "clusters"]}
+      mapboxApiAccessToken={
+        "pk.eyJ1Ijoib2xlZHliZWRva2tlbiIsImEiOiJja3ljb3ZvMnYwcmdrMm5vZHZtZHpqcWNvIn0.9-uQhH-WQmh-IwrA6gNtUg"
+      }
+      clickRadius={2}
+      onViewportChange={setViewport}
+      mapStyle="mapbox://styles/mapbox/streets-v11"
+      onClick={ShowMore}
+      onMouseEnter={onMouseEnter}
+      ref={mapRef}
+    >
+      <Source
+        id="stasjoner"
+        type="geojson"
+        data={sources}
+        generateId={true}
+        cluster={true}
+        clusterMaxZoom={14}
+        clusterRadius={50}
+      >
         <Layer {...clusterLayer} />
         <Layer {...clusterCountLayer} />
-        <Layer {...unclusteredPointLayer} paint={{"circle-color":color,"circle-radius":8,'circle-stroke-width': 1,'circle-stroke-color': '#fff'}}/>
+        <Layer {...unclusteredPointLayer}/>
       </Source>
       {selectedSource && (
-          <Popup
-            longitude={hoverInfo.longitude}
-            latitude={hoverInfo.latitude}
-            closeButton={false}
-            className="county-info"
-          >
-            {selectedCounty}
-          </Popup>
-        )}
+        <Popup
+          longitude={hoverInfo.long}
+          latitude={hoverInfo.lat}
+          closeButton={false}
+          className="county-info"
+        >
+          {selectedSource}
+        </Popup>
+      )}
     </ReactMapGL>
-
   );
 };
 
