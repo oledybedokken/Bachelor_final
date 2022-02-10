4538012bd69a7b3ec10c137725225e9abace432f
Mo <mussa80banjai@users.noreply.github.com>
2022-02-10 17:47:23 +0100

    Importing npm csv greia

--- Server.js
+++ Server.js
@@ -6,6 +6,8 @@ app.use(cors());
 const db = require("./db");
 const fetch = require('node-fetch');
 var GeoJSON = require('geojson');
+const fs = require("fs");
+const fastcsv = require("fast-csv");
 const port = process.env.PORT || 3001;
 
 // Få alle plasser
@@ -45,11 +47,11 @@ fetch('https://frost.met.no/sources/v0.jsonld?types=SensorSystem&country=Norge',
     .then(async(data)=>{
         try{
             await db.query("DROP TABLE IF EXISTS sources;")
-            await db.query("CREATE TABLE sources(id VARCHAR(10) PRIMARY KEY UNIQUE NOT NULL,type VARCHAR(50),name VARCHAR(60) NOT NULL,shortName VARCHAR(50),country VARCHAR(70) NOT NULL,countryCode VARCHAR(80),long VARCHAR(90) NOT NULL,lat VARCHAR(100) NOT NULL,geog geography(point) NOT NULL,valid_from TIMESTAMP,valid_to TIMESTAPM,county VARCHAR(100) ,countyId INT ,municipality VARCHAR(100) ,municipalityId INT);")
+            await db.query("CREATE TABLE sources(id VARCHAR(10) PRIMARY KEY UNIQUE NOT NULL,type VARCHAR(50),name VARCHAR(60) NOT NULL,shortName VARCHAR(50),country VARCHAR(70) NOT NULL,countryCode VARCHAR(80),long VARCHAR(90) NOT NULL,lat VARCHAR(100) NOT NULL,geog geography(point) NOT NULL,valid_from TIMESTAMP,county VARCHAR(100) ,countyId INT ,municipality VARCHAR(100) ,municipalityId INT);")
             data.data.map(async(source)=>{
                 if(source.geometry && source.geometry){
                 let Point = `POINT(${source.geometry.coordinates[0]} ${source.geometry.coordinates[1]})`
-                const results =await db.query("INSERT INTO sources(id,type,name,shortName,country,countryCode,long,lat,geog,valid_from,valid_to,county,countyId,municipality,municipalityId) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) returning name;",[source.id,source.type,source.name,source.shortName,source.country,source.countryCode,source.geometry.coordinates[0],source.geometry.coordinates[1],Point,source.validFrom,source.validTo,source.county,source.countyId, source.municipality,source.municipalityId])
+                const results =await db.query("INSERT INTO sources(id,type,name,shortName,country,countryCode,long,lat,geog,valid_from,county,countyId,municipality,municipalityId) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) returning name;",[source.id,source.type,source.name,source.shortName,source.country,source.countryCode,source.geometry.coordinates[0],source.geometry.coordinates[1],Point,source.validFrom,source.county,source.countyId, source.municipality,source.municipalityId])
         }
         })
     }
@@ -161,6 +163,32 @@ app.get("/api/v1/sources/near/", async (req, res) =>{
      }
 })
 
+async function FetchDataInntekt(){
+    fetch('https://frost.met.no/sources/v0.jsonld?types=SensorSystem&country=Norge',{
+        method:"get",
+        body: JSON.stringify(),
+        headers:{ Authorization: 'Basic YjVlNmEzODEtZmFjNS00ZDA4LWIwNjktODcwMzBlY2JkNTFjOg==' }
+    })
+        .then(res =>res.json())
+        .then(async(data)=>{
+            try{
+                await db.query("DROP TABLE IF EXISTS sources;")
+                await db.query("CREATE TABLE sources(id VARCHAR(10) PRIMARY KEY UNIQUE NOT NULL,type VARCHAR(50),name VARCHAR(60) NOT NULL,shortName VARCHAR(50),country VARCHAR(70) NOT NULL,countryCode VARCHAR(80),long VARCHAR(90) NOT NULL,lat VARCHAR(100) NOT NULL,geog geography(point) NOT NULL,valid_from TIMESTAMP,county VARCHAR(100) ,countyId INT ,municipality VARCHAR(100) ,municipalityId INT);")
+                data.data.map(async(source)=>{
+                    if(source.geometry && source.geometry){
+                    let Point = `POINT(${source.geometry.coordinates[0]} ${source.geometry.coordinates[1]})`
+                    const results =await db.query("INSERT INTO sources(id,type,name,shortName,country,countryCode,long,lat,geog,valid_from,county,countyId,municipality,municipalityId) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) returning name;",[source.id,source.type,source.name,source.shortName,source.country,source.countryCode,source.geometry.coordinates[0],source.geometry.coordinates[1],Point,source.validFrom,source.county,source.countyId, source.municipality,source.municipalityId])
+            }
+            })
+        }
+        catch(err){
+            console.log(err)
+        }
+        const value ="Hei"
+        return (value)})
+        .catch(error=>console.log(error))
+    }
+    
 
 
 app.listen(port, () => {
