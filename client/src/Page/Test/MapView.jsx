dfdc7f69d87ced33ae703c92ecc939767d0a9c79
oledybedokken <69799492+oledybedokken@users.noreply.github.com>
2022-02-15 20:41:38 +0100

    update

--- client/src/Page/Test/MapView.jsx
+++ client/src/Page/Test/MapView.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import MapGL, { Source, Layer } from 'react-map-gl';
 const MapView = ({data}) => {
+    
+    const [viewport, setViewport] = React.useState({
+        longitude: 10.757933,
+        latitude: 59.91149,
+        zoom: 12,
+        bearing: 0,
+        pitch: 0,
+      });
+      
     return (<>
         <MapGL
+        {...viewport}
         width="100%"
         height="100%"
         initialViewState={{
             longitude: 10.757933,
             latitude: 59.91149,
-            zoom: 3,
+            zoom: 12,
         }}
+        onViewportChange={setViewport}
         mapStyle="mapbox://styles/mapbox/dark-v9"
         mapboxApiAccessToken='pk.eyJ1Ijoib2xlZHliZWRva2tlbiIsImEiOiJja3ljb3ZvMnYwcmdrMm5vZHZtZHpqcWNvIn0.9-uQhH-WQmh-IwrA6gNtUg'
         ></MapGL>
--- client/src/Page/Test/index.js
+++ client/src/Page/Test/index.js
@@ -14,18 +14,14 @@ const Test = () => {
   const currentTime = Math.floor(Date.now() / 1000);
   const[weatherData,setWeatherData] = useState(null)
   const [min,setMin] = useState(null)
-  //Velge 10 sources
-    //import the sources || DONE
-    //Gjøre slik at man kan velge max antall sources || DONE
   //Lage ferdig Kart for en source
-    //Ta inn kart
-    //Bli ferdig med LAbel
-  //Hente inn data for active sources når knappen blir trykka
-    //Lage funksjon som sender 10 sources i request
-    //Lage en NodeJs funksjon som fetcher 1 data og returnerer en array basert på den dataen
-    //Lage en Nodejs funksjon som fetcher 10 dataer og returner en massiv array
+    //Ta inn kart || DONE
+    //Bli ferdig med Label
   //HeatMap
     //Implementere kart og heatmap
+  //Utvide slikt at man kan ta inn opptil 10 sources  
+  //Lage funksjon som sender 10 sources i request
+  //Lage en Nodejs funksjon som fetcher 10 dataer og returner en massiv array
   const handleTimeChange = (event, newValue) => {
     setSpesifiedTime(newValue);
   };
@@ -60,14 +56,19 @@ const Test = () => {
     return (sources.features.filter((source)=>source.properties.county.toLowerCase() === filterBy.toLowerCase()))
 }
 async function GetData(){
-  const inpWeatherData = await SourceFinder.get("/weatherdata",);
-  setWeatherData(inpWeatherData.data.data.value.data)
-  setMin(Math.floor(new Date(inpWeatherData.data.data.value.data[0].referenceTime).getTime()/1000))
+  const inpWeatherData = await SourceFinder.get("/weatherdata",{params:{
+    id:valgteSources[0]
+  }});
+  console.log(inpWeatherData.data.data.plass)
+  setWeatherData(inpWeatherData.data.data.plass)
+  setMin(Math.floor(new Date(inpWeatherData.data.data.plass.features[0].properties.referenceTime).getTime()/1000))
+  setSpesifiedTime(Math.floor(new Date(inpWeatherData.data.data.plass.features[0].properties.referenceTime).getTime()/1000))
 }
 function SortByDate(){
-  const newArray = weatherData.filter((dag)=>dag.referenceTime.split("T")[0]===unixTimeToFrostTime(spesifiedTime))
-  console.log(newArray)
-  return weatherData.filter((dag)=>dag.referenceTime.split("T")[0]===unixTimeToFrostTime(spesifiedTime))
+  const newArray = weatherData.features.filter((dag)=>dag.properties.referenceTime.split("T")[0]===unixTimeToFrostTime(spesifiedTime))
+  if(newArray.length>0){
+  console.log(newArray[0])}
+  return newArray
 }
 if (loading){
   return <p>Loading..</p>
@@ -76,7 +77,11 @@ if (loading){
     {sources&&
     fylker.map((fylke)=><TableForFylke key={fylke.fylkesnavn} fylke = {fylke.fylkesnavn} valgteSources={valgteSources} setValgteSources={setValgteSources} fylkeListe={FilterAndSort(fylke.fylkesnavn)}/>)
     }
-    <Box sx={{ justifyContent:"center",display:"flex"}}>
+    <Box sx={{ justifyContent:"center",display:"flex"}}>    
+    </Box>
+    {valgteSources&&<><h1>{valgteSources.length}</h1>
+    <Typography>{timeNow()}</Typography>
+    <Button onClick={GetData}>Hent data</Button>
     <Slider
       getAriaLabel={() => 'Date range'}
       value={spesifiedTime}
@@ -86,15 +91,17 @@ if (loading){
       sx={{width:"500px"}}
       max={currentTime}
       min={min}
-    />
-    <Typography>{timeNow()}</Typography>
-    </Box>
-    {valgteSources&&<><h1>{valgteSources.length}</h1><Button onClick={GetData}>Hent data</Button></>}
+    /></>}
     {weatherData&&
     SortByDate().length>0?
-    SortByDate().map((dag)=><div><p>Mean Temp:{dag.observations[0].value}</p><p>dato:{dag.referenceTime}</p></div>):<p>loading</p>}
+    SortByDate().map((dag)=><div>Hello</div>):<p>loading</p>}
     {weatherData&&<Container maxWidth="lg" sx={{height:"500px"}}><MapView data={weatherData}/></Container>}
   </div>;
 };
 
 export default Test;
+ //Velge 10 sources
+    //import the sources || DONE
+    //Gjøre slik at man kan velge max antall sources || DONE
+//Hente inn data for active sources når knappen blir trykka 
+    //Lage en NodeJs funksjon som fetcher 1 data og returnerer en array basert på den dataen || DONE  
