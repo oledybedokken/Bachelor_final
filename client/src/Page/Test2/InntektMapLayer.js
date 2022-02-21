527c30704b2a2973de2267d2333d83b7b11b5c43
Mo <mussa80banjai@users.noreply.github.com>
2022-02-21 16:54:18 +0100

    Colors kinda done

--- client/src/Page/Test2/InntektMapLayer.js
+++ client/src/Page/Test2/InntektMapLayer.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
-const InntektMapLayer = () => {
-  return (
-    <div>InntektMapLayer</div>
-  )
+export const inntektmapLayer = {
+  id: "heatmap",
+  maxzoom:12,
+  type: "circle",
+  'paint': {
+      'circle-color': [
+          'interpolate',
+          ['linear'],
+          ['get', 'value'],
+          -10,
+          '#000137',
+          -5,
+          '#02198B',
+          0,
+          '#fff',
+          5,
+          '#ffffe0',
+          10,
+          '#FFD580',
+          15,
+          '#FF8c00',
+          30,
+          '#7F3121'
+      ],
+      'circle-opacity': 0.75,
+      'circle-radius':20,
+  }
 }
-
-export default InntektMapLayer
