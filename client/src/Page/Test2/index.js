2fea829b305564a7e0cf7933f9d66b886476721f
Mo <mussa80banjai@users.noreply.github.com>
2022-02-22 13:09:07 +0100

    Importing the kommuner

--- client/src/Page/Test2/index.js
+++ client/src/Page/Test2/index.js
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import { Button,Slider,Box, Typography,Container } from '@mui/material';
+import { useEffect } from 'react';
+import SourceFinder from '../../Apis/SourceFinder';
+import { SourceContext } from "../../context/SourceContext";
+import TableForFylke from './TableForFylke';
+import MapView from './MapView';
+import Chart from './Chart';
+
+
 
 const Test2 = () => {
+  const {sources,setSources}= useContext(SourceContext)
+  const [kommuner,setKommuner]= useState(null);
+  const [loading,setLoading] = useState(true)
+  const [valgteSources,setValgteSources] = useState(null)
+  const [spesifiedTime,setSpesifiedTime] = useState([1549697922])
+  const currentTime = Math.floor(Date.now() / 1000);
+  const[inntektData,setInntektData] = useState(null)
+  const [min,setMin] = useState(null)
+  useEffect(() => {
+    const fetchData = async ()=>{
+      try{
+        const sources = await SourceFinder.get("/sources");
+        const fylker = await SourceFinder.get("/kommuner");
+        setKommuner(kommuner.data.data.kommuner)
+        setSources(sources.data.data.plass)
+      } catch(err){
+        console.log(err)
+      }
+    }
+    if(sources === null){
+      fetchData()
+    }
+   setLoading(false)
+}, []);
   return (
     <div>Test2</div>
   )
