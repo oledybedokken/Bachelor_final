18d0a806d8a3775743b38b49167f62044489b692
Mo <mussa80banjai@users.noreply.github.com>
2022-02-22 12:58:37 +0100

    Adding test2 on routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,6 +3,7 @@ const Home = React.lazy(()=>import("./Page/Home"));
 const Map = React.lazy(()=>import("./Page/Map"));
 const SourceTesting = React.lazy(()=>import("./Page/SourceTesting"));
 const Test = React.lazy(()=>import("./Page/Test"));
+const Test2 = React.lazy(()=>import("./Page/Test2"));
 const routes=[
     {
         path:"/",element:Home
@@ -12,5 +13,6 @@ const routes=[
     },
     {path:"/sourcetesting",element:SourceTesting},
     {path:"/test",element:Test},
+    {path:"/test2",element:Test2},
 ]
 export default routes;
