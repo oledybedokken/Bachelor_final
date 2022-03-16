4ae91c6d20501d904c86ca17a9ba5e7421d2affb
oledybedokken <69799492+oledybedokken@users.noreply.github.com>
2022-03-16 18:18:31 +0100

    Update

--- client/src/Page/Inntekt/index.js
+++ client/src/Page/Inntekt/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState,useMemo,useCallback,useRef } from 'react'
-import {PieChart, Pie, Legend, Tooltip, ResponsiveContainer} from 'recharts';
 import { Typography,Container,Box,Slider,InputLabel,Select,MenuItem,FormControl } from '@mui/material'
 import SourceFinder from '../../Apis/SourceFinder'
 import MapGL, { Source, Layer,Popup } from 'react-map-gl';
@@ -151,20 +150,7 @@ const Inntekt = () => {
         )}
         </MapGL>
         </Box>
-        <ResponsiveContainer width="100%" height="100%">
-        <PieChart width={400} height={400}>
-          <Pie
-            dataKey="value"
-            isAnimationActive={false}
-            data={data}
-            cx="50%"
-            cy="50%"
-            outerRadius={80}
-            fill="#8884d8"
-            label
-          />
-        </PieChart>
-      </ResponsiveContainer>
+        
     </>
     
   )
