7528f619bbd3ba20dfdb76dbc86ce21cfd773559
oledybedokken <69799492+oledybedokken@users.noreply.github.com>
2022-03-16 18:12:57 +0100

    Update

---
+++ client/src/Page/Inntekt/Chart.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Chart = ({data}) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={400}>
+          <Pie
+            dataKey="value"
+            isAnimationActive={false}
+            data={data}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          />
+        </PieChart>
+      </ResponsiveContainer>
+  )
+}
+
+export default Chart
