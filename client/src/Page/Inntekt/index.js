613466faed88dbfdeee3fdfc0a22b823b7933fdd
Mo <mussa80banjai@users.noreply.github.com>
2022-03-16 17:57:52 +0100

    PieChart

--- client/src/Page/Inntekt/index.js
+++ client/src/Page/Inntekt/index.js
@@ -151,7 +151,22 @@ const Inntekt = () => {
         )}
         </MapGL>
         </Box>
+        <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={400}>
+          <Pie
+            dataKey="value"
+            isAnimationActive={false}
+            data={data}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          />
+        </PieChart>
+      </ResponsiveContainer>
     </>
+    
   )
 }
 
