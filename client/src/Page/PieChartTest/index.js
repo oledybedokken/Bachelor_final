be7df5d67d3740600ba8cfa87d17da1070285dc8
Mo <mussa80banjai@users.noreply.github.com>
2022-03-19 18:18:23 +0100

    Testing med enkelt verdier

---
+++ client/src/Page/PieChartTest/index.js
@@ -0,0 +1,41 @@
+import React, {
+    useEffect,
+    useState,
+    useMemo,
+} from "react";
+
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+
+
+const PieChartTest = () => {
+    const data01 = [
+        { name: 'Group A', value: 400 },
+        { name: 'Group B', value: 300 },
+        { name: 'Group C', value: 300 },
+        { name: 'Group D', value: 200 },
+        { name: 'Group E', value: 278 },
+        { name: 'Group F', value: 189 },
+    ];
+
+return (
+    <div>
+        <ResponsiveContainer width="100%" height="100%">
+            <PieChart width={400} height={400}>
+                <Pie
+                    dataKey="value"
+                    isAnimationActive={false}
+                    data={data01}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label
+                />
+                <Tooltip />
+            </PieChart>
+        </ResponsiveContainer>
+    </div>
+);
+};
+
+export default PieChartTest;
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,11 +2,13 @@ import React from 'react'
 const Home = React.lazy(()=>import("./Page/Home"));
 const Værdata = React.lazy(()=>import("./Page/Værdata"));
 const Inntekt = React.lazy(()=>import("./Page/Inntekt"))
+const PieChartTest = React.lazy(() => import("./Page/PieChartTest"));
 const routes=[
     {
         path:"/",element:Home
     },
     {path:"/vaermap",element:Værdata},
-    {path:"/inntekt",element:Inntekt}
+    {path:"/inntekt",element:Inntekt},
+    {path:"/ptest",element:PieChartTest}
 ]
 export default routes;
