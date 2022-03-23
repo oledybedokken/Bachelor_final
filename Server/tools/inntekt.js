6eb4d875cdf049e05b81aeedcdb4b642071fcb8c
oledybedokken <69799492+oledybedokken@users.noreply.github.com>
2022-03-23 13:27:50 +0100

    Optimization

--- Server/tools/inntekt.js
+++ Server/tools/inntekt.js
@@ -9,69 +9,44 @@ function LeseData() {
   //info.slice(1)
   console.time("StartTime")
   let start = 0
-  let newLinke = info.slice(1).filter(linje=>linje[linje.length-1] !== ".")
-  info.slice(1).filter(linje=>linje[linje.length-1] !== ".").map((linje) => {
+  info.slice(1).filter(linje=>linje[linje.length-1] !== ".").slice(0,1000).map((linje) => {
     //if (linje[linje.length - 1] !== ".") {
       let testLinje = linje.split(";");
-        if (objectArray.filter((data)=>data["navn"] === testLinje[0].replaceAll('"', "").split(" ")[1]).filter((data) => data.aar === testLinje[3].replaceAll('"', "")).filter((data)=>data.Husholdningtype===testLinje[1].split(/\s(.+)/)[1].replaceAll('"', "")).length > 0 && objectArray.length > 0) {
+        if (objectArray.some(data=>data["navn"] === testLinje[0].replaceAll('"', "").split(" ")[1] && data.aar === parseInt(testLinje[3].replaceAll('"', "")) && data.Husholdningtype===testLinje[1].split(/\s(.+)/)[1].replaceAll('"', ""))){
+        //if (objectArray.filter((data)=>data["navn"] === testLinje[0].replaceAll('"', "").split(" ")[1]).filter((data) => data.aar === testLinje[3].replaceAll('"', "")).filter((data)=>data.Husholdningtype===testLinje[1].split(/\s(.+)/)[1].replaceAll('"', "")).length > 0 && objectArray.length > 0) {
           //Her finnes både Navn og Året fra før
           for(var i = start; i<objectArray.length; i++){
-            if(objectArray[i]["navn"]===testLinje[0].replaceAll('"', "").split(" ")[1] && objectArray[i]["aar"]===testLinje[3].replaceAll('"', "")){
+            if(objectArray[i]["navn"]===testLinje[0].replaceAll('"', "").split(" ")[1] && objectArray[i]["aar"]===parseInt(testLinje[3].replaceAll('"', "")) && objectArray[i]["Husholdningtype"]===testLinje[1].split(/\s(.+)/)[1].replaceAll('"', "")){
                 if (testLinje[2].replaceAll('"', "") ==="Samlet inntekt, median (kr)") {
-                    objectArray[i]["bruttoInntekt"] = testLinje[4];
+                    objectArray[i]["bruttoInntekt"] = parseInt(testLinje[4]);
                     start = i
                     break;
                   } else if (testLinje[2].replaceAll('"', "") ==="Inntekt etter skatt, median (kr)") {
-                    objectArray[i]["nettoInntekt"] = testLinje[4];
+                    objectArray[i]["nettoInntekt"] = parseInt(testLinje[4]);
                     start = i
                     break;
                   } else if (testLinje[2].replaceAll('"', "") === "Antall husholdninger") {
-                    objectArray[i]["AntallHusholdninger"] = testLinje[4];
+                    objectArray[i]["AntallHusholdninger"] = parseInt(testLinje[4]);
                     start = i
                     break;
                   }
                 }
           }
         }
-          /*objectArray.map((pos)=>{
-            if(pos["navn"]===testLinje[0].replaceAll('"', "").split(" ")[1] && pos["aar"]===testLinje[3].replaceAll('"', "")){
-            if (testLinje[2].replaceAll('"', "") ==="Samlet inntekt, median (kr)") {
-                pos["bruttoInntekt"] = testLinje[4];
-                return;
-              } else if (testLinje[2].replaceAll('"', "") ==="Inntekt etter skatt, median (kr)") {
-                pos["nettoInntekt"] = testLinje[4];
-                return;
-              } else if (testLinje[2].replaceAll('"', "") === "Antall husholdninger") {
-                pos["AntallHusholdninger"] = testLinje[4];
-                return;
-              }
-            }
-          })
-          /* objectArray.filter((data)=>data["navn"]=== testLinje[0].replaceAll('"', "").split(" ")[1]).filter((data) => data.aar === testLinje[3].replaceAll('"', "")).map((pos) => {
-              if (testLinje[2].replaceAll('"', "") ==="Samlet inntekt, median (kr)") {
-                pos["bruttoInntekt"] = testLinje[4];
-              } else if (testLinje[2].replaceAll('"', "") ==="Inntekt etter skatt, median (kr)") {
-                pos["nettoInntekt"] = testLinje[4];
-              } else if (testLinje[2].replaceAll('"', "") === "Antall husholdninger") {
-                pos["AntallHusholdninger"] = testLinje[4];
-              }
-            }); 
-        }*/
     else {
           let createObject = {};
           createObject["navn"] = testLinje[0].replaceAll('"', "").split(" ")[1];
-          createObject["kommuneNr"] = testLinje[0].replaceAll('"', "").split(" ")[0];
+          createObject["kommuneNr"] = parseInt(testLinje[0].replaceAll('"', "").split(" ")[0]);
           createObject["HusholdningId"] = testLinje[1].replaceAll('"', "").split(" ")[0];
-          createObject["aar"] = testLinje[3].replaceAll('"', "");
-          let husarray = testLinje[1].split(/\s(.+)/)[0];
+          createObject["aar"] = parseInt(testLinje[3].replaceAll('"', ""));
           createObject["Husholdningtype"] = testLinje[1].split(/\s(.+)/)[1].replaceAll('"', "");
           let errorHandling = false;
           if (testLinje[2].replaceAll('"', "") === "Samlet inntekt, median (kr)") {
-            createObject["bruttoInntekt"] = testLinje[4];
+            createObject["bruttoInntekt"] = parseInt(testLinje[4]);
           } else if (testLinje[2].replaceAll('"', "") ==="Inntekt etter skatt, median (kr)") {
-            createObject["nettoInntekt"] = testLinje[4];
+            createObject["nettoInntekt"] = parseInt(testLinje[4]);
           } else if (testLinje[2].replaceAll('"', "") === "Antall husholdninger") {
-            createObject["AntallHusholdninger"] = testLinje[4];
+            createObject["AntallHusholdninger"] = parseInt(testLinje[4]);
           } else {
             errorHandling = true;
           }
@@ -107,6 +82,30 @@ function SammenSlaaing(){
         }
     })
     console.log(nyVerdi) */
+    /*objectArray.map((pos)=>{
+            if(pos["navn"]===testLinje[0].replaceAll('"', "").split(" ")[1] && pos["aar"]===testLinje[3].replaceAll('"', "")){
+            if (testLinje[2].replaceAll('"', "") ==="Samlet inntekt, median (kr)") {
+                pos["bruttoInntekt"] = testLinje[4];
+                return;
+              } else if (testLinje[2].replaceAll('"', "") ==="Inntekt etter skatt, median (kr)") {
+                pos["nettoInntekt"] = testLinje[4];
+                return;
+              } else if (testLinje[2].replaceAll('"', "") === "Antall husholdninger") {
+                pos["AntallHusholdninger"] = testLinje[4];
+                return;
+              }
+            }
+          })
+          /* objectArray.filter((data)=>data["navn"]=== testLinje[0].replaceAll('"', "").split(" ")[1]).filter((data) => data.aar === testLinje[3].replaceAll('"', "")).map((pos) => {
+              if (testLinje[2].replaceAll('"', "") ==="Samlet inntekt, median (kr)") {
+                pos["bruttoInntekt"] = testLinje[4];
+              } else if (testLinje[2].replaceAll('"', "") ==="Inntekt etter skatt, median (kr)") {
+                pos["nettoInntekt"] = testLinje[4];
+              } else if (testLinje[2].replaceAll('"', "") === "Antall husholdninger") {
+                pos["AntallHusholdninger"] = testLinje[4];
+              }
+            }); 
+        }*/
 }
 SammenSlaaing()
 /* console.log(tomtArray) */
