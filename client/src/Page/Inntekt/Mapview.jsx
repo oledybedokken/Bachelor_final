6c8394f5602fbf9f2ce65355402608ef5510ba5c
Mo <mussa80banjai@users.noreply.github.com>
2022-03-29 16:01:07 +0200

    It was TypeScript

--- client/src/Page/Inntekt/Mapview.jsx
+++ client/src/Page/Inntekt/Mapview.jsx
@@ -92,18 +92,7 @@ const Mapview = ({data,DrawerInnhold,InntektSlider}) => {
       </Popup>
     )}
     {/* Experiment */}
-    {/* (['left', 'right', 'top', 'bottom'] as const).map((anchor) => (
-      <React.Fragment key={anchor}>
-        <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-        <Drawer
-          anchor={anchor}
-          open={state[anchor]}
-          onClose={toggleDrawer(anchor, false)}
-        >
-          {list(anchor)}
-        </Drawer>
-      </React.Fragment>
-    )) */}
+    
 
   </MapGL>
   <InntektSlider></InntektSlider>
