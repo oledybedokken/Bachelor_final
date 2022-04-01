3371580f48d0565879c6121653085c0ec1639729
oledybedokken <69799492+oledybedokken@users.noreply.github.com>
2022-04-01 12:34:20 +0200

    Test

--- tools/test.js
+++ tools/test.js
@@ -7,53 +7,36 @@ const sammenSlaaing = require("../sammenSlaaing.js");
 function test(){
     return JSONstat(url).then(main);
 }
+function startsWithNumber(str) {
+    return /^\d/.test(str);
+  }
 const KommuneReformen = sammenSlaaing.KommuneSammenSlaaing();
 async function main(j){
     const kommuner = await fetch("https://ws.geonorge.no/kommuneinfo/v1/kommuner").then(response => response.json()).then(data => { return data });
     var ds=j.Dataset(0);
+    let ssbKommuner=Object.entries(ds.__tree__.dimension.Region.category.label).reduce((acc, [key, value]) => (acc[value] = parseInt(key), acc), {})
     let array = ds.toTable( { type : "arrobj" } ,function( d ){
         if ( d.value!==null){
+            d.RegionNumber = ssbKommuner[d.Region]
            return d;
         }
-     })
-/*      fs.writeFileSync('./data2.json', JSON.stringify(array, null, 2), 'utf-8');
- */    let test=array
-    let test2 = []
-    test.map((lol)=>{
-        const arr = lol.Region.split(" ")
-        if(arr.length>1){
-            if(arr[arr.length-1].charAt(arr[arr.length-1].length - 1)===")"){
-                test2.push(arr)
-                if(arr.slice(0, -1)[arr.slice(0, -1).length-1][0]==="(" ){
-                    if(arr.slice(0, -2)[1] ==="-"|| arr.slice(0, -2)[arr.slice(0, -2).length-2]==="-"){
-                        lol["Region"] = KommuneTest(arr.slice(0, -1))
-                    } 
-                    else{
-                        lol["Region"] = arr.slice(0, -2).join(" ")
-                    }  
-                }
-                else{
-                    if(arr.slice(0, -1)[1] ==="-"|| arr.slice(0, -1)[arr.slice(0, -1).length-2]==="-"){
-                        lol["Region"] = KommuneTest(arr.slice(0, -1),kommuner)
-                    }
-                    else{ lol["Region"] = arr.slice(0, -1).join(" ")}
-                }
-            }
-            else if(arr[1]==="-"){
-                lol["Region"] = KommuneTest(arr,kommuner)
+     });     
+    array.map((sted)=>{
+        let RegionSplit =sted.Region.split("(")
+        sted.Region = RegionSplit[0].trim()
+        if(RegionSplit.length>1){
+            if(startsWithNumber(RegionSplit[1].split("-")[1])){
+                sted.gyldigTil = parseInt(RegionSplit[1].split("-")[1].slice(0,-1));   
             }
             else{
-                
-                if(arr[arr.length-1]==="kommune"){
-                    lol["Region"]=arr[0]
-                }
+                sted.tilhører=RegionSplit[1].slice(0,-1)
             }
         }
-    })
-/*     fs.writeFileSync('./data3.json', JSON.stringify(test3, null, 2), 'utf-8');
+    });
+    fs.writeFileSync('./data2.json', JSON.stringify(array, null, 2), 'utf-8');
+/*    fs.writeFileSync('./data3.json', JSON.stringify(test3, null, 2), 'utf-8');
  */    /* console.log(array.filter(data=>data.value!==null)) */
     let verider = SammenSlaaing(test)
-     
     return verider
   }
 function SammenSlaaing(alleVerider){
@@ -100,32 +83,6 @@ function SammenSlaaing(alleVerider){
         let result = [...newKommunerSammen,...alleVerider]
     /* let result = [...newKommuner,...arr] */
      return result
-}
-function KommuneTest(kommuneArray,kommuner){
-    let aktivKommune = null
-    kommuneArray.map((kommuneArr)=>{
-        if (kommuner.some((kommune) => kommune.kommunenavnNorsk === kommuneArr)) {
-            aktivKommune = kommuneArr;
-            return
-          }
-    })
-    if(aktivKommune===null){
-    for(let i = 0;i<kommuneArray.length;i=i+2){
-        for (let y = 0; y < KommuneReformen.length; y++) {
-            KommuneReformen[y].GammelKommune.split(",").map((gammelKommune) => {
-            let currKommune = gammelKommune.replace(/^\s+/g, '');
-            if(currKommune===kommuneArray[i]){
-                aktivKommune = kommuneArray[i]
-                return
-            }
-        })
-        }
-        if(aktivKommune!==null){
-            break
-        }
     }
-}
-    return aktivKommune
-}
 /* test() */
 module.exports={test}
