8b7b18cdda729654cd3fab74442ce42866ed1144
Mo <mussa80banjai@users.noreply.github.com>
2022-04-19 01:53:23 +0200

    Fixing route for the help

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,10 +4,12 @@ const OldHome = React.lazy(()=>import("./Page/Home"));
 const weatherData = React.lazy(()=>import("./Page/Værdata"));
 const SsbData = React.lazy(() => import("./Page/SsbData"));
 const SsbDataId = React.lazy(() => import("./Page/SsbVisualization"));
+const Help = React.lazy(() => import("./Page/Help"));
 const routes=[
     {path:"/",element:Home},
     {path:"/weather",element:weatherData},
     {path:"/ssb",element:SsbData},
     {path:"/ssb/:id",element:SsbDataId},
+    {path:"/help",element:Help},
 ]
 export default routes;
