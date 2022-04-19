d8aa165bd7fbbe0786a2e5957733999c766209d2
Mo <mussa80banjai@users.noreply.github.com>
2022-04-19 16:19:49 +0200

    Ident

--- client/src/Page/Help/MainBar.jsx
+++ client/src/Page/Help/MainBar.jsx
@@ -10,23 +10,23 @@ import { Link } from 'react-router-dom';
 const MainBar = () => {
   const colorMode = React.useContext(ColorModeContext);
   return (
-      <>
-    <AppBar position='static' color="transparent" elevation={0}>
-        <Toolbar disableGutters>
-          <Container sx={{display:"flex", alignItems:"center", justifyContent:"space-between"}} disableGutters maxWidth="">
-            <Box sx={{justifyContent:"left"}}>
-              <Image src={colorMode.mode==="dark"?LogoDark:LogoLight} duration={300} sx={{maxWidth:"200px"}}></Image>
-            </Box>
-            <Box sx={{flex:"1",display:"flex",justifyContent:"right"}}>
-                <FormControlLabel
-                  control={<Daynightswitch sx={{ m: 1 }} checked={colorMode.mode==="dark"?true:false} onChange={colorMode.toggleColorMode} />}
-                  label=""
-                />
-                <Button key={"Home"} to={"/"} component={Link} sx={{fontSize:"1.5em",color:"white",whiteSpace:"nowrap",":hover":{color:"#fff"},fontWeight:700}} variant="contained" >Back</Button>
-            </Box>
-          </Container>
-        </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position='static' color="transparent" elevation={0}>
+          <Toolbar disableGutters>
+            <Container sx={{display:"flex", alignItems:"center", justifyContent:"space-between"}} disableGutters maxWidth="">
+              <Box sx={{justifyContent:"left"}}>
+                <Image src={colorMode.mode==="dark"?LogoDark:LogoLight} duration={300} sx={{maxWidth:"200px"}}></Image>
+              </Box>
+              <Box sx={{flex:"1",display:"flex",justifyContent:"right"}}>
+                  <FormControlLabel
+                    control={<Daynightswitch sx={{ m: 1 }} checked={colorMode.mode==="dark"?true:false} onChange={colorMode.toggleColorMode} />}
+                    label=""
+                  />
+                  <Button key={"Home"} to={"/"} component={Link} sx={{fontSize:"1.5em",color:"white",whiteSpace:"nowrap",":hover":{color:"#fff"},fontWeight:700}} variant="contained" >Back</Button>
+              </Box>
+            </Container>
+          </Toolbar>
+      </AppBar>
     </>
     
   )
