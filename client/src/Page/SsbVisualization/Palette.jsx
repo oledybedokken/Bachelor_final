24e616a5b6449cee500839d6e1279eac4c9ac940
Mo <mussa80banjai@users.noreply.github.com>
2022-04-23 03:42:23 +0200

    Console log funker

--- client/src/Page/SsbVisualization/Palette.jsx
+++ client/src/Page/SsbVisualization/Palette.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import ColorBox from '../../Components/ColorBox'
 const Palette = () => {
     const colors =[{label:"NO DATA",color:"#121212"},{label:"0%",color:"hsl(0, 100%, 24%)"},{label:"20%",color:"hsl(4, 100%, 40%)"},{label:"40%",color:"hsl(11, 91%, 49%)"},{label:"60%",color:"hsl(16, 99%, 59%)"},{label:"80%",color:"hsl(21, 100%, 67%)"},{label:"100%",color:"hsl(29, 100%, 74%)"}]
-  return (
+
+    return (
       <>
       {colors &&
     <Grid display="flex" flexDirection={"column"}>{colors.map((input)=>{
         return(
-        <Grid item key={input.label}>
-            <ColorBox color={input.color} label={input.label}></ColorBox>
+        <Grid item key={input.label} onClick = {() => console.log(input.label)}>
+            <ColorBox color={input.color} label={input.label} ></ColorBox>
         </Grid>)
     })}</Grid>}
     </>
