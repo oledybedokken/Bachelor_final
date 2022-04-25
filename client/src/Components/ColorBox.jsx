726fe7a7e43272c292c42f239883c8fccc391425
Mo <mussa80banjai@users.noreply.github.com>
2022-04-25 02:26:33 +0200

    I DONT EVEN KNOW WHAT TO DO!!!!

--- client/src/Components/ColorBox.jsx
+++ client/src/Components/ColorBox.jsx
@@ -1,10 +1,24 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import React from "react";
 
-const ColorBox = ({color,label}) => {
+const ColorBox = ({ color, label, setChosenColor }) => {
   return (
-    <Box width={"100px"} height={"50px"} sx={{backgroundColor:color,display:"flex",alignItems:"center", justifyContent:"center"}}><Typography color="#fff" align="center" variant ="h6">{label}</Typography></Box>
-  )
-}
+    <Box
+      width={"100px"}
+      height={"50px"}
+      sx={{
+        backgroundColor: color,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+      onClick={() => setChosenColor(color)}
+    >
+      <Typography color="#fff" align="center" variant="h6">
+        {label}
+      </Typography>
+    </Box>
+  );
+};
 
-export default ColorBox
+export default ColorBox;
