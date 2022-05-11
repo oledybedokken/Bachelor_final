b14d0f745d6cfe4654ba3a2235eac5efcc736c53
Mo <mussa80banjai@users.noreply.github.com>
2022-05-11 04:44:01 +0200

    Rart spørsmål

--- client/src/Page/Help/FAQ/data.js
+++ client/src/Page/Help/FAQ/data.js
@@ -1,10 +1,14 @@
 export const data = [
     {
-        question: 'What is 1+1?',
-        answer: '4'
+        question: 'About DataViz',
+        answer: 'Is a web application that visualizes and plots data that has both a geographical and a temporal component.'
     },
     {
-        question: 'What is crocodile?',
-        answer: 'An Animal'
-    }
+        question: 'Data sources',
+        answer: 'The sources of data are ssb.no and met.api.no'
+    },
+    {
+        question: 'Who created the web application?',
+        answer: 'It was created by Fatima Matieva, Mussa Banjai and Ole Dybedokken.'
+    },
 ]
