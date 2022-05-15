6bfd35b7e4399dea7dfa36ee5b6d4c40da872678
Mo <mussa80banjai@users.noreply.github.com>
2022-05-15 08:33:51 +0200

    Homepage basically ferdig

--- client/src/Page/Home/SecondSection/SlideShow.jsx
+++ client/src/Page/Home/SecondSection/SlideShow.jsx
@@ -6,6 +6,7 @@ import Bergen from '../../../Assets/SlideShow/bergen-4208335_960_720.jpeg'
 import 'swiper/css';
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import { Link as RouterLink } from 'react-router-dom'
 
 // import required modules
 const SlideShow = () => {
@@ -13,7 +14,7 @@ const SlideShow = () => {
     const cards = [
         { url: "https://images.pexels.com/photos/6882085/pexels-photo-6882085.jpeg?cs=srgb&dl=pexels-yaroslav-shuraev-6882085.jpg&fm=jpg", label: "From freezing winters to warm summers, see the history of norwegian temperatures.", heading: "Temperatures" },
         { url: "https://images.pexels.com/photos/3384695/pexels-photo-3384695.jpeg?cs=srgb&dl=pexels-stein-egil-liland-3384695.jpg&fm=jpg", label: "Heavy wind speeds are common in Norway. Checkout out the history of wind speeds in Norway over the last 50 years.", heading: "Wind" },
-        { url: "Bergen", label: "Ever been to Bergen, well then you know about the rain. Check out the history of humidity for the last 50 years in Norway.", heading: "Humidity" }]
+        { url: Bergen, label: "Ever been to Bergen, well then you know about the rain. Check out the history of humidity for the last 50 years in Norway.", heading: "Humidity" }]
 
     return (
         <Swiper
@@ -38,7 +39,7 @@ const SlideShow = () => {
             <Paper sx={{backgroundImage: 'url(' + props.item.url + ')', height: "500px", backgroundSize: 'cover',backgroundPosition: 'center',display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"Center",boxShadow: "0px 4px 4px 0px #00000040,inset 0 0 0 1000px rgba(0,0,0,.5)"}} >
                 <Typography variant="h3" color="#fff" sx={{fontWeight:700}}>{props.item.heading}</Typography>
                 <Typography variant="body" color="#fff" sx={{pt:"2%"}}>{props.item.label}</Typography>
-                <Button className="CheckButton" variant="contained" sx={{mt:"5%"}}>
+                <Button className="CheckButton" variant="contained" sx={{mt:"5%"}} component={RouterLink} to={"/weather"}>
                     Check it out!
                 </Button>
             </Paper>
