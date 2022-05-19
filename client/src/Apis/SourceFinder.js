324c2118a10cc7a141e044030cd7b18a14fcc6a3
oledybedokken <69799492+oledybedokken@users.noreply.github.com>
2022-05-19 03:46:01 +0200

    Nodeenvupdate

--- client/src/Apis/SourceFinder.js
+++ client/src/Apis/SourceFinder.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 import axiosRetry from "axios-retry";
-
+const baseURL = process.env.NODE_ENV === "production"? "/api/v1":"http://localhost:3005/api/v1"
 const SourceFinder = axios.create({
-  baseURL:
-    process.env.NODE_ENV !== "production"
-      ? "/api/v1"
-      : "http://localhost:3005/api/v1"
+  baseURL,
 });
 
 axiosRetry(SourceFinder, { retries: 3 });
